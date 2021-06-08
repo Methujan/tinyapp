63aa62eaa039638c8397365b3acef8bdd7341dae
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-08 03:32:06 +0000

    shortURL redirects to longURL

--- express_server.js
+++ express_server.js
@@ -37,6 +37,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   if (req) {
     const newShortURL = generateRandomString();
