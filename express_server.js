bb1769364271451fd5252953dbf276f3f00ed966
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-09 19:24:01 +0000

    cookies for register

--- express_server.js
+++ express_server.js
@@ -14,6 +14,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 
 
 function generateRandomString() {
@@ -107,7 +120,20 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 })
 
-
+//register
+app.post('/register', (req,res) => {
+  const newRandomID = generateRandomString();
+  users[newRandomID] = {
+    newRandomID: {
+      id: newRandomID,
+      email: req.body.email,
+      password: req.body.password
+    }
+  }
+  console.log(users);
+  res.cookie('user_id', newRandomID)
+  res.redirect('/urls')
+})
 
 
 
